6596cf5d529096cf67a512981b7d58c7f04e9012
Oskar <jaosni@gmail.com>
2020-12-04 15:11:54 +0100

    Added outer for loop for solving part two.

--- AoC20/Day3/aoc_three.py
+++ AoC20/Day3/aoc_three.py
@@ -29,13 +29,11 @@ def get_next_column(last, step=3):
     if last == MAX_COL_INDEX:
         return step - 1
     elif last + step > MAX_COL_INDEX:
-        return (last + step) - MAX_COL_INDEX-1
+        return (last + step) - MAX_COL_INDEX - 1
     else:
         return last + step
 
 
-number_of_open_visited = 0
-number_of_trees_visited = 0
 # Count trees following path right 3 - down 1
 if __name__ == "__main__":
     # Read input
@@ -46,22 +44,33 @@ if __name__ == "__main__":
     # sled_map = dummy_map
     MAX_COL_INDEX = len(sled_map[0]) - 1
 
-    print(sled_map[0])
-    print(MAX_COL_INDEX)
-    # For every step :
-    col = 0
-    for row in sled_map:
-        # print(row)
-        # Go to correct column
-        what_have_we_here = row[col]
-        col = get_next_column(col, step=3)
-        if what_have_we_here == '#':
-            number_of_trees_visited += 1
-        elif what_have_we_here == '.':
-            number_of_open_visited += 1
-        else:
-            print("SOMETHING IS WRONG HERE!")
+    step_size = [1, 3, 5, 7, 1]
+    step_down_size = [1, 1, 1, 1, 2]
+    trees_on_path = []
 
-    print("Number of trees visited: {}".format(number_of_trees_visited))
-    print("Number of open visited: {}".format(number_of_open_visited))
-    # ANS 105 is too low
+    for i, step in enumerate(step_size):
+        # For every step :
+        number_of_open_visited = 0
+        number_of_trees_visited = 0
+        col = 0
+        row_step = step_down_size[i]
+        for j in range(0, len(sled_map), row_step):
+            # print(row)
+            # Go to correct column
+            what_have_we_here = sled_map[j][col]
+            col = get_next_column(col, step=step)
+            if what_have_we_here == '#':
+                number_of_trees_visited += 1
+            elif what_have_we_here == '.':
+                number_of_open_visited += 1
+            else:
+                print("SOMETHING IS WRONG HERE!")
+
+        trees_on_path.append(number_of_trees_visited)
+
+    print("Number of trees visited: {}".format(trees_on_path))
+    mul = 1
+    for num in trees_on_path:
+        mul *= num
+
+    print(mul)
