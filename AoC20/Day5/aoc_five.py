19962c0cd6a64b765a9f4de45f6b885605d42616
Oskar <jaosni@gmail.com>
2020-12-05 11:13:56 +0100

    Added method to get a binary string of the input character string. Solved for the test case as well as part one.

--- AoC20/Day5/aoc_five.py
+++ AoC20/Day5/aoc_five.py
@@ -1,5 +1,3 @@
-
-
 # Binary toggling bits
 # For rows: 'F' - "de"-set bit (i.e make it zero)
 #           'B' - set bit (i.e make it one)
@@ -12,6 +10,32 @@
 # row 14, col 7, ID 119
 # row 102, col 4, ID 820
 
+# Part one: Find the highest seat ID
+# BBFFBBF RLL
+
+def map_string_to_bin(str):
+    binary_str = '0b'
+    for i, char in enumerate(str):
+        if char == 'F' or char == 'L':
+            binary_str += '0'
+        elif char == 'B' or char == 'R':
+            binary_str += '1'
+        # else
+        # do nothing, escape character here
+    return binary_str
+
+
 
 if __name__ == "__main__":
-    print("hello world")
+    seat_id = []
+    f = open("day5.txt", "r")
+    for lines in f:
+        to_row = map_string_to_bin(lines[0:7])
+        to_col = map_string_to_bin(lines[7:])
+        row = int(to_row, 2)
+        col = int(to_col, 2)
+        seat_id.append(8*row + col)
+    print(seat_id)
+    print(max(seat_id))
+
+
