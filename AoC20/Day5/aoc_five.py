b6eab0b839da5e8f19c48f6074161639999e70d6
Oskar <jaosni@gmail.com>
2020-12-05 10:54:33 +0100

    Added structure for solving part 1 of day five

---
+++ AoC20/Day5/aoc_five.py
@@ -0,0 +1,17 @@
+
+
+# Binary toggling bits
+# For rows: 'F' - "de"-set bit (i.e make it zero)
+#           'B' - set bit (i.e make it one)
+# For columns: 'L' - "de"-set bit (i.e make it zero)
+#              'R' - set bit (i.e make it one)
+# Seat ID : 8*row + column
+
+# Ans to test:
+# row 70, col 7, ID 567
+# row 14, col 7, ID 119
+# row 102, col 4, ID 820
+
+
+if __name__ == "__main__":
+    print("hello world")
