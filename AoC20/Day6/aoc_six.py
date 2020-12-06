d9715dd73e3cf34e309a67c7d0ba33bb5fc68612
oskarnilsson <jaosni@gmail.com>
2020-12-06 13:52:16 +0100

    Refactor to a better solution. Starting with part two

--- AoC20/Day6/aoc_six.py
+++ AoC20/Day6/aoc_six.py
@@ -1,25 +1,29 @@
 
-ans_sheet = {
-        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0,
-        'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0,
-        'w': 0, 'x': 0, 'y': 0, 'z': 0
-    }
+#ans_sheet = {
+#        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0,
+#        'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0,
+#        'w': 0, 'x': 0, 'y': 0, 'z': 0
+#    }
 
 if __name__ == "__main__":
     group_ans = []
-    ans = ans_sheet.copy()
 
+    ans = {}
     f = open("day6.txt", "r")
     for line in f:
         if line == "\n":
             print("New group:")
             group_ans.append(ans)
-            ans = dict(ans_sheet)
+
+            ans = {}
         else:
             print(line)
             for char in line:
                 if char != '\n':
-                    ans[char] = 1
+                    if char in ans:
+                        ans[char] = ans[char] + 1
+                    else:
+                        ans[char] = 1
 
     group_ans.append(ans)
 
